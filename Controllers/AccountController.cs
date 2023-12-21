1f1ba6dd8082b8d6ba314b81caada0f3a801da3e
SourLemon <lemon4sour@gmail.com>
2023-12-21 04:18:55 +0300

    profile page

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Numerics;
+using System.Security.Claims;
 using iDEA.Entity;
 using iDEA.Models;
 using Microsoft.AspNetCore.Authentication;
@@ -13,37 +14,31 @@ namespace iDEA.Controllers
 
         private readonly DataContext _context;
 
-        public AccountController(DataContext context) {
-            _context = context;
-        }
-
-        [HttpGet]
-        public IActionResult Login()
+        public AccountController(DataContext context)
         {
-            if(User.Identity!.IsAuthenticated) {
-                return RedirectToAction("Index", "Account");
-            }
-            return View();
+            _context = context;
         }
         [HttpPost]
         public async Task<IActionResult> Login(LoginModel model)
         {
-            if(User.Identity!.IsAuthenticated) {
+            if (User.Identity!.IsAuthenticated)
+            {
                 return RedirectToAction();
             }
 
-            if (!ModelState.IsValid) {
+            if (!ModelState.IsValid)
+            {
                 ModelState.AddModelError("", "Invalid username or password.");
                 ViewBag.Invalid = true;
-                return View("~/Views/Home/Index.cshtml",model);
+                return View("~/Views/Home/Index.cshtml", model);
             }
-            
+
             var isUser = _context.People.FirstOrDefault(x => x.Username == model.Username && x.Password == model.Password);
             if (isUser == null)
             {
                 ModelState.AddModelError("", "Invalid username or password.");
                 ViewBag.Invalid = true;
-                return View("~/Views/Home/Index.cshtml",model);
+                return View("~/Views/Home/Index.cshtml", model);
             }
 
             var userClaims = new List<Claim>
@@ -53,13 +48,15 @@ namespace iDEA.Controllers
                 new(ClaimTypes.GivenName, isUser.Name + " " + isUser.Surname ?? "")
             };
 
-            if (_context.Lecturers.FirstOrDefault(x => x.ID == isUser.ID) is null) {
+            if (_context.Lecturers.FirstOrDefault(x => x.ID == isUser.ID) is null)
+            {
                 userClaims.Add(new Claim(ClaimTypes.Role, "admin"));
             }
 
             var claimsIdentity = new ClaimsIdentity(userClaims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-            var authProperties = new AuthenticationProperties {
+            var authProperties = new AuthenticationProperties
+            {
                 IsPersistent = true
             };
 
@@ -68,17 +65,58 @@ namespace iDEA.Controllers
 
             return RedirectToAction("Index", "Account");
         }
-        
-        public async Task<IActionResult> Logout() {
+
+        public async Task<IActionResult> Logout()
+        {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            ViewBag.GivenName = User.FindFirstValue(ClaimTypes.GivenName);
-            return View();
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            int AccountID = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            var model = new UserDataModel
+            {
+                Name = User.FindFirstValue(ClaimTypes.GivenName),
+
+                ID = AccountID,
+
+                Department = (from combined in
+                      (from student in _context.Students
+                        where student.ID == AccountID
+                        select new { student.ID, student.Department })
+                        .Union
+                        (from lecturer in _context.Lecturers
+                         where lecturer.ID == AccountID
+                         select new { lecturer.ID, lecturer.Department })
+                              select combined.Department)
+                  .FirstOrDefault(),
+
+                IsStudent = _context.Students.Any(x => x.ID == AccountID),
+
+                IsLecturer = _context.Lecturers.Any(x => x.ID == AccountID),
+
+                GPA = (float)_context.TakenCourses.Where(x => x.PersonID == AccountID)
+                .Average(x => x.Point >= 90 ? 4.0 :
+                x.Point >= 80 ? 3.0 :
+                x.Point >= 70 ? 2.0 :
+                x.Point >= 60 ? 1.0 : 0.0),
+
+                Credit = (int)_context.TakenCourses.Where(x => x.PersonID == AccountID)
+                .Join(_context.Courses,
+                takenCourse => takenCourse.CourseID,
+                course => course.ID,
+                (takenCourse, course) => course.Credit)
+                .Sum()
+            };
+            return View(model);
         }
     }
 }
---
+++ Models/UserDataModel.cs
@@ -0,0 +1,12 @@
+﻿namespace iDEA.Models {
+    public class UserDataModel{
+        public string? Name { get; set; }
+        public int ID { get; set; }
+        public string? Department { get; set; }
+        public bool IsStudent { get; set; }
+        public bool IsLecturer { get; set; }
+        public float GPA { get; set; }
+        public int Credit { get; set; }
+    }
+}
+
